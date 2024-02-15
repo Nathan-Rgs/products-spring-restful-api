9964c3ce605b57e9f1c217862226ca67aae7c003
nathan-epiousion <nathan.santos@epiousion.com.br>
2024-02-15 11:24:01 -0300

    Crud functions with hateoas

---
+++ src/test/java/com/example/productsSpringApi/ProductsSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productsSpringApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
